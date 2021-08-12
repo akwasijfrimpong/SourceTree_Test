9ea45d9fdcdb16ebef2783dd3cd51b5032b14832
akwasijfrimpong <akwasijfrimpong@gmail.com>
2021-08-12 19:41:26 +1200

    hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sourcetree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World hehe!");
+        }
+    }
+}
